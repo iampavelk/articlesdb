709ccb25528b97591311449b42e826c7ba529d11
KPR <130840048+iampavelk@users.noreply.github.com>
2023-11-17 16:27:12 +0200

    Revert "poetry add black"
    This reverts commit f018af27c1a4780f65abb01eff240164a0b20968.

--- scraper.py
+++ scraper.py
@@ -1,26 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-
-def scrape_article_text(article_url):
-    response = requests.get(article_url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    article_text = soup.find("div", class_="article-formatted-body")
-    return article_text
-
-
-URL = "https://habr.com/ru/hubs/python/articles/"
+URL = 'https://habr.com/ru/hubs/python/articles/'
 
 results = requests.get(URL)
 page = results.content
 
 soup = BeautifulSoup(page, "html.parser")
-articles = soup.find_all("article")
+articles = soup.find(class_="tm-articles-list").find_all('article',class_="tm-articles-list__item")
 for article in articles:
-    article_title = article.find(class_="tm-title__link")
-    article_url = f'https://habr.com{article_title["href"]}'
-    pulishing_time = article.find(class_="tm-article-datetime-published").time
-    pulishing_time = pulishing_time["title"]
-    article_content = scrape_article_text(article_url)
-    print(article_url, article_title.text, pulishing_time, article_content)
-    break
+    title = article.find(class_="tm-title__link")
+    link = title['href']
+    time = article.find(class_="tm-article-datetime-published").time
+    time= time['title']
+    print(f'https://habr.com{link}', title.text, time)
