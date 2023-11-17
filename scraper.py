e7aee46c345fc0b44d4c6bbd48ff8bbc903e082f
KPR <130840048+iampavelk@users.noreply.github.com>
2023-11-17 16:36:50 +0200

    added scrape_article_text function

--- scraper.py
+++ scraper.py
@@ -1,16 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = 'https://habr.com/ru/hubs/python/articles/'
+
+def scrape_article_text(article_url):
+    response = requests.get(article_url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    article_text = soup.find("div", class_="article-formatted-body")
+    return article_text
+
+
+URL = "https://habr.com/ru/hubs/python/articles/"
 
 results = requests.get(URL)
 page = results.content
 
 soup = BeautifulSoup(page, "html.parser")
-articles = soup.find(class_="tm-articles-list").find_all('article',class_="tm-articles-list__item")
+articles = soup.find_all("article")
 for article in articles:
-    title = article.find(class_="tm-title__link")
-    link = title['href']
-    time = article.find(class_="tm-article-datetime-published").time
-    time= time['title']
-    print(f'https://habr.com{link}', title.text, time)
+    article_title = article.find(class_="tm-title__link")
+    article_url = f'https://habr.com{article_title["href"]}'
+    publishing_time = article.find(class_="tm-article-datetime-published").time
+    publishing_time = publishing_time["title"]
+    article_content = scrape_article_text(article_url)
+    print(article_url, article_title.text, publishing_time, article_content)
+    break
