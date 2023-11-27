d0c5438dd362f6d2fa707ac7391b6d237dd237f1
KPR <130840048+iampavelk@users.noreply.github.com>
2023-11-27 15:08:19 +0200

    added "save news: function to habr.com parser

--- app/core/parsers/habr.py
+++ app/core/parsers/habr.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 from utils import get_html
+from app.articles.news import News
+from sqlmodel import Session, select
+from app.core.db_sync import engine
 
 
 def scrape_article_text(article_url):
@@ -8,6 +11,21 @@ def scrape_article_text(article_url):
     soup = BeautifulSoup(html, "html.parser")
     article_text = soup.find("div", class_="article-formatted-body").decode_contents()
     return article_text
+
+
+def save_news(article_title, article_url, publishing_time, article_content):
+    with Session(engine) as session:
+        query = select(News).where(News.url == article_url)
+        news_exists = session.exec(query).all()
+        if not news_exists:
+            new_news = News(
+                title=article_title,
+                url=article_url,
+                published=publishing_time,
+                content=article_content,
+            )
+            session.add(new_news)
+            session.commit()
 
 
 URL = "https://habr.com/ru/hubs/python/articles/"
@@ -19,8 +37,9 @@ articles = soup.find_all("article")
 for article in articles:
     article_title = article.find(class_="tm-title__link")
     article_url = f'https://habr.com{article_title["href"]}'
+    article_title = article_title.text
     publishing_time = article.find(class_="tm-article-datetime-published").time
     publishing_time = publishing_time["title"]
     article_content = scrape_article_text(article_url)
-    print(article_url, article_title.text, publishing_time, article_content)
-    break
+    # print(article_url, article_title, publishing_time, article_content)
+    save_news(article_title, article_url, publishing_time, article_content)
