defe0f19e0a893eb6e3369a51a7cd8e4eb5f70dc
KPR <130840048+iampavelk@users.noreply.github.com>
2023-11-27 12:44:23 +0200

    added get news and post news routes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
+from sqlmodel import SQLModel, Session, select
 
 from app import settings
 from app.core.models import HealthCheck
+from app.articles.news import News
+from app.core.db_sync import engine
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
 
 app = FastAPI(
     title=settings.project_name,
@@ -12,13 +20,32 @@ app = FastAPI(
 )
 
 
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
 @app.get("/", response_model=HealthCheck, tags=["status"])
-async def health_check():
+def health_check():
     return {
         "name": settings.project_name,
         "version": settings.version,
         "description": settings.description,
     }
+
+
+@app.get("/news/")
+def get_news():
+    with Session(engine) as session:
+        newss = session.exec(select(News)).all()
+        return newss
+
+
+@app.post("/news/", response_model=News)
+def post_news(news: News):
+    with Session(engine) as session:
+        session.add(news)
+        session.commit()
 
 
 if __name__ == "__main__":
